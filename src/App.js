2144416ee62385bf24fb4d19df3d71ba5ce5abc1
Keegan Bond <keeganbond123@gmail.com>
2022-12-01 23:35:35 -0500

    created video

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.scss";
 import FilterBar from "./components/FilterBar/FilterBar";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Video from "./components/Video/Video";
 function App() {
     return (
         <>
             <Header />
             <FilterBar />
+            <Video />
             <Footer />
         </>
     );
