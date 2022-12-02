a1436f09d0b4c52d1ac4f85ac049b9988f734439
Keegan Bond <keeganbond123@gmail.com>
2022-12-01 22:05:39 -0500

    created footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import FilterBar from './components/FilterBar/FilterBar';
+import FilterBar from "./components/FilterBar/FilterBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
-        <FilterBar/>
-    )
+        <>
+            <FilterBar />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import "./Footer.scss";
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <ul className="Footer__list">
+                <li className="Footer__list-item">Help center</li>
+                <li className="Footer__list-item"> Send Feedback</li>
+                <li className="Footer__list-item"> Terms of service</li>
+                <li className="Footer__list-item"> Privacy Policy</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Footer;
