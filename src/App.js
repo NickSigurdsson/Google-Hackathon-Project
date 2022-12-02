a222630316251b5a3152f69c962c3a4161665075
NickSigurdsson <nicholassigurdsson@gmail.com>
2022-12-02 10:48:38 -0500

    filter bar functionality

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import "./App.scss";
 import FilterBar from "./components/FilterBar/FilterBar";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import SearchBar from "./components/SearchBar/SearchBar";
+// import SearchBar from "./components/SearchBar/SearchBar";
 function App() {
     return (
         <>
             <Header />
-            <SearchBar/>
             <FilterBar />
             <Footer />
         </>
     );
 }
-
 export default App;
