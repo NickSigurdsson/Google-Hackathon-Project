4ea7da90de709cadc2860625310a733e79ad0aa4
Keegan Bond <keeganbond123@gmail.com>
2022-12-01 21:50:41 -0500

    created header, found asset images

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import logo from "../../assets/logos/google-svgrepo-com.svg";
-import profile from "../../assets/icons/profile_pic.png";
-import candyBox from "../../assets/icons/candy-box.png";
+import profile from "../../assets/icons/profile_pic1.png";
+import candyBox from "../../assets/icons/menu-svgrepo-com.svg";
 import "./Header.scss";
 function Header(props) {
     return (
@@ -10,9 +10,17 @@ function Header(props) {
                 src={logo}
                 alt="google logo"
             ></img>
-            <div>
-                <img src={profile} alt="google profile, letter M"></img>
-                <img src={candyBox} alt="candy box 3x3 square"></img>
+            <div className="Header__icon-container">
+                <img
+                    className="Header__candy-box"
+                    src={candyBox}
+                    alt="candy box 3x3 square"
+                ></img>
+                <img
+                    className="Header__profile-picture"
+                    src={profile}
+                    alt="google profile, letter M"
+                ></img>
             </div>
         </div>
     );
